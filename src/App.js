4c42b9aac422e85750dc70e21650c6d3b24e6310
Thomas ZAWISZA <thomaszawisza@Thomass-MacBook-Pro.local>
2021-05-23 13:24:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () =>{
+  const badString = "javascript:alert('Hacked');"
+  const anothBadString = "<img onerror='alert(\"Hacked Again !\");' src='invalid-image'/>"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href={badString}>Check my website yo </a>
+
+      <div dangerouslySetInnerHTML={{"__html": anothBadString}}></div>
+
+
     </div>
   );
 }
